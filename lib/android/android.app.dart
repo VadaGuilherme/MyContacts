474e57b2513f5547d78e0cfdce0c5829f4a7353a
Guilherme Vada <mail@guilhermevada.com.br>
2020-08-08 22:43:22 -0300

    Created splash

--- lib/android/android.app.dart
+++ lib/android/android.app.dart
@@ -1,25 +1,14 @@
 import 'package:contacts/android/styles.dart';
+import 'package:contacts/android/views/splash.view.dart';
 import 'package:flutter/material.dart';
 
 class AndroidApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Contacts',
-      debugShowCheckedModeBanner: false,
-      theme: androidTheme(),
-      home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 40,
-            ),
-            Text(
-              "Testando a fonte",
-            ),
-          ],
-        ),
-      ),
-    );
+        title: 'Contacts',
+        debugShowCheckedModeBanner: false,
+        theme: androidTheme(),
+        home: SplashView());
   }
 }
--- lib/android/views/splash.view.dart
+++ lib/android/views/splash.view.dart
@@ -3,6 +3,29 @@ import 'package:flutter/material.dart';
 class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: double.infinity,
+          ),
+          Icon(
+            Icons.fingerprint,
+            size: 72,
+            color: Theme.of(context).accentColor,
+          ),
+          Text(
+            "Autenticação",
+            style: TextStyle(
+              fontSize: 24,
+              color: Theme.of(context).accentColor,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
