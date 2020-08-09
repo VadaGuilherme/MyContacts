4fbecdb8baf536c1f79e44aa3385f7e1542c0d0d
Guilherme Vada <mail@guilhermevada.com.br>
2020-08-08 23:27:29 -0300

    Changed home view and avatar contact

--- lib/android/views/home.view.dart
+++ lib/android/views/home.view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomeView extends StatelessWidget {
   @override
@@ -16,6 +17,28 @@ class HomeView extends StatelessWidget {
             color: Theme.of(context).primaryColor,
           ),
         ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Container(
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(48),
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://avatars2.githubusercontent.com/u/16677074?s=460&u=f6ad8051fd3e0680bc35c048490fc22f0d6a0bb2&v=4"))),
+            ),
+            title: Text("Guilherme Vada"),
+            subtitle: Text("(16) 99300-4623"),
+            trailing: FlatButton(
+              onPressed: () {},
+              child: Icon(Icons.chat, color: Theme.of(context).primaryColor),
+            ),
+          ),
+        ],
       ),
     );
   }
